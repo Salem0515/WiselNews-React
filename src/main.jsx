9db44023aa43e4335faac3b062238e9eec3df97f
Salem0515 <167808611+Salem0515@users.noreply.github.com>
2024-07-10 17:16:28 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+        < BrowserRouter basename= "/WiselNews-React/">
     <App />
+              </BrowserRouter>
+
   </React.StrictMode>,
 )
